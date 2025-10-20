0dbbe797330a21b6f38d066a02cccbbf6efb8980
Abdulmohsen-99 <446107134@student.ksu.edu.sa>
2025-10-20 13:17:42 +0300

    Add files via upload

---
+++ LibrarySimulator.java
@@ -0,0 +1,17 @@
+package LibrarySimulator;
+
+/*
+ * [Abdulmohsen Alabdulkaream] ID: 446107134
+ * [Rayan Alaseed] ID: 446104338
+ * [Abdulelah Alhoshan] ID: 446101982
+ */
+
+import java.util.Scanner;
+
+public class LibrarySimulator {
+static Scanner input = new Scanner(System.in);
+public static void main(String[] args) {
+	
+	
+}
+}
