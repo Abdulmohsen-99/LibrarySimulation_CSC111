19e85300cc95aa7b168fd4089c87c1aee86607a4
AbdulelahAlhoshan <abdulalahsaad@gmail.com>
2025-10-20 17:02:10 +0300

    Update LibrarySimulator.java

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -8,24 +8,46 @@ package LibrarySimulator;
 import java.util.Scanner;
 
 public class LibrarySimulator {
+	
 public static void main(String[] args) {
+	
 	Scanner input = new Scanner(System.in);
-
-
-boolean exitprogram = false;
-while(!exitprogram) {
-	System.out.println("\n=== Library System ===");
-	System.out.println("1.  userlogin");
-	System.out.println("2.  Adminlogin");
-	System.out.println("3.  Exit");
-	System.out.println("Choose: ");
-	int choice =input.nextInt();
-	if (choice==1) {
+	
+	while (true) {
 		
-	//هنا شغل ريان ال سعيد (  واجهة المستخدم   )
-	//   لاتنسى تحط طريق رجعة للقائمة	
+		System.out.println("Welcome to The Library Simulator!");
+		System.out.println("Please select an option: ");
+		System.out.println("1. Login as Ali (ID: 101)");
+		System.out.println("2. Login as Mohammed (ID: 102)");
+		System.out.println("3. Login as Salman (ID: 103)");
+		System.out.println("4. Login as Administrator");
+		System.out.println("5. Exit the program");
+		System.out.print("Enter your choice (1-5): ");
+		
+	int choice = input.nextInt();
 	
+	switch (choice) {
+	
+	case 1 :
+		System.out.println("User Ali selected");
+		break;
+	case 2 :
+		System.out.println("User Mohammed selected");
+		break;
+	case 3 :
+		System.out.println("User Salman selected");
+		break;
+	case 4 :
+		System.out.println("Administrator selected");
+		break;
+	case 5 :
+		System.out.println("Thank you for using the Library Simulator. Goodbye!");
+		break;
+	default :
+		System.out.println("Error: Invalid choice. Please enter a number between 1 and 5.");
+			
+	}	
 	}
-	
-}}}
+}
+}
 
