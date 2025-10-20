c1aba095b00c767f11044967c6d4570ae118608e
Abdulmohsen-99 <446107134@student.ksu.edu.sa>
2025-10-20 13:54:44 +0300

    Implement main method for library system
    Added main method and user interaction loop.

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -1,17 +1,31 @@
-package LibrarySimulator;
-
 /*
  * [Abdulmohsen Alabdulkaream] ID: 446107134
  * [Rayan Alaseed] ID: 446104338
  * [Abdulelah Alhoshan] ID: 446101982
  */
+package LibrarySimulator;
 
 import java.util.Scanner;
 
 public class LibrarySimulator {
-static Scanner input = new Scanner(System.in);
 public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+
+boolean exitprogram = false;
+while(!exitprogram) {
+	System.out.println("\n=== Library System ===");
+	System.out.println("1.  userlogin");
+	System.out.println("2.  Adminlogin");
+	System.out.println("3.  Exit");
+	System.out.println("Choose: ");
+	int choice =input.nextInt();
+	if (choice==1) {
+		
+	//هنا شغل ريان ال سعيد (  واجهة المستخدم   )
+	//   لاتنسى تحط طريق رجعة للقائمة	
 	
+	}
 	
-}
-}
+}}}
+
