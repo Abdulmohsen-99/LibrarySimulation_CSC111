74b1ccf2f713ce70db0d7583abd860baf7053d8b
AbdulelahAlhoshan <abdulalahsaad@gmail.com>
2025-10-20 17:56:45 +0300

    Update LibrarySimulator.java

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -16,38 +16,34 @@ public static void main(String[] args) {
 	while (true) {
 		
 		System.out.println("Welcome to The Library Simulator!");
-		System.out.println("Please select an option: ");
-		System.out.println("1. Login as Ali (ID: 101)");
-		System.out.println("2. Login as Mohammed (ID: 102)");
-		System.out.println("3. Login as Salman (ID: 103)");
-		System.out.println("4. Login as Administrator");
-		System.out.println("5. Exit the program");
-		System.out.print("Enter your choice (1-5): ");
+		System.out.println("Please select an option:");
+		System.out.println("1. Login as User");
+		System.out.println("2. Login as Administrator");
+		System.out.println("3. Exit the program");
+		System.out.print("Enter your choice (1-3): ");
 		
 	int choice = input.nextInt();
 	
-	switch (choice) {
-	
-	case 1 :
-		System.out.println("User Ali selected");
-		break;
-	case 2 :
-		System.out.println("User Mohammed selected");
-		break;
-	case 3 :
-		System.out.println("User Salman selected");
-		break;
-	case 4 :
-		System.out.println("Administrator selected");
-		break;
-	case 5 :
-		System.out.println("Thank you for using the Library Simulator. Goodbye!");
-		break;
-	default :
-		System.out.println("Error: Invalid choice. Please enter a number between 1 and 5.");
-			
-	}	
+    if (choice == 1) {
+    // ريان يضيف اسماء المستخدمين و اي دي كل واحد فيهم تحت الاف ذي داخل القوسين	
+    	
 	}
+    
+    else if (choice == 2) {
+    	
+    	System.out.println("Administrator selected.");
+    }
+   
+    else if (choice == 3) {
+    	
+    	System.out.println("Thank you for using the Library Simulator. Goodbye!");
+    }
+    
+    else {
+    	
+    	System.out.println("Error: Invalid choice. Please enter a number between 1 and 3.");
+    }
+}
 }
 }
 
